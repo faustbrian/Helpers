669ae1c1a3ffcd794bc7cc4180527e63ac89af9f
DraperStudio <hello@draperstudio.tech>
2016-04-24 21:29:20 +0200

    Add validate method

--- src/Laravel/Validator.php
+++ src/Laravel/Validator.php
@@ -18,3 +18,26 @@ if (!function_exists('validator')) {
         return app('validator');
     }
 }
+
+if (!function_exists('validate')) {
+    /**
+     * Validate some data.
+     *
+     * @param string|array $fields
+     * @param string|array $rules
+     *
+     * @return bool
+     */
+    function validate($fields, $rules)
+    {
+        if (!is_array($fields)) {
+            $fields = ['default' => $fields];
+        }
+    
+        if (!is_array($rules)) {
+            $rules = ['default' => $rules];
+        }
+    
+        return \Validator::make($fields, $rules)->passes();
+    }
+}
