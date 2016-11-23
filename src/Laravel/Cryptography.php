c8c13fe4093fa9955071b716d36d65a35487b971
Brian Faust <hello@brianfaust.de>
2016-11-23 10:39:49 +0000

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- src/Laravel/Cryptography.php
+++ src/Laravel/Cryptography.php
@@ -1,13 +1,22 @@
 <?php
 
-if (!function_exists('crypto')) {
+/*
+ * This file is part of Helpers.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+if (! function_exists('crypto')) {
     function crypto()
     {
         return app('encrypter');
     }
 }
 
-if (!function_exists('hasher')) {
+if (! function_exists('hasher')) {
     function hasher()
     {
         return app('hash');
