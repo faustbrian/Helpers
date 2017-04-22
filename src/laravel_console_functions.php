1b5d123e16857619b1d7c70bb40627de063cd49c
Brian Faust <hello@brianfaust.de>
2017-04-22 08:44:09 +0300

    Try to fix the mess StyleCI makes

--- src/laravel_console_functions.php
+++ src/laravel_console_functions.php
@@ -1,13 +1,6 @@
 <?php
 
-/*
- * This file is part of Helpers.
- *
- * (c) Brian Faust <hello@brianfaust.de>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+
 
 if (!function_exists('artisan')) {
     function artisan($command = null, array $parameters = [])
