55961604fd1a0fd32f514f191619bc130ae37765
Brian Faust <faustbrian@users.noreply.github.com>
2017-04-22 08:44:23 +0300

    Apply fixes from StyleCI (#8)
    [ci skip] [skip ci]

--- src/laravel_image_functions.php
+++ src/laravel_image_functions.php
@@ -1,6 +1,13 @@
 <?php
 
-
+/*
+ * This file is part of Helpers.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 if (!function_exists('extract_colours')) {
     function extract_colours($file, $amount = 6, $type = 'png')
--- src/type_conversion_functions.php
+++ src/type_conversion_functions.php
@@ -1,6 +1,13 @@
 <?php
 
-
+/*
+ * This file is part of Helpers.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 if (!function_exists('array_to_object')) {
     function array_to_object($data)
